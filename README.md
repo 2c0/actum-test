53a17f9784e3137bcf56f206e2e42fe46a5713cc
Hai Ha Nguyen <haicorp87@gmail.com>
2017-09-24 15:12:37 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# actum-test
-This is an Actum repo test
+# PHO DAT PROJECT #
+
+## How to setup the project ##
+
+We use gulp as build tool
+source folder for frontend code is root/source and build folder is in template directory
+eslint and sass-lint uses airbnb codestyle presets (airbnb base without react).
+
+## Frontend Stack ##
+
+bootstrap: 3.3.7 (https://github.com/twbs/bootstrap-sass),
+jquery: 3.1.1 (https://jquery.com),
+autoprefixer: 6.7.3 (https://github.com/postcss/autoprefixer),
+gulp: 3.9.1 (http://gulpjs.com)
+eslint (gulp-eslint): 3.0.1 (https://github.com/adametry/gulp-eslint)
+sass-lint (gulp-sass-lint): 1.3.2 (https://github.com/sasstools/gulp-sass-lint)
+browsersync (https://www.browsersync.io/docs#installation)
+
+## Testing ##
+
+For javascript unit testing we use mocha as a test framework (https://mochajs.org/) and chai as additional assertion library (http://chaijs.com/) which allows us to write and chain nice BDD assertions. We are testing via testrunner.html file. For testing your code just import the source and test code on defined place in the file and then open it in the browser.
+Linting
+
+Linting is the process of running a program that will analyse code for potential errors. We use linters for javascript (eslint) and for sass its (gulp-sass-lint), both are runned via gulp. Eslint configuration is located in .eslintrc in root project folder and sass-lint in .sass-lint.yml in root project folder.
---
+++ gulpfile.js
@@ -0,0 +1,118 @@
+// 1 - declaring variables
+
+//var themename = 'phodat';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var eslint = require('gulp-eslint');
+var sassLint = require('gulp-sass-lint');
+var compass = require('gulp-compass');
+//var babel = require("gulp-babel");
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+
+
+// Name of workng theme folder
+
+
+
+// Sources + Destination variables
+var srcRoot = 'source/**/*.*';
+//var fontsSrc = "source/fonts/**/*.*";
+//var fontsDest = "www/wp-content/themes/phodat/build/fonts";
+
+var sassSrc = "source/sass/**/*.scss";
+var sassDest = ""; //example www/wp-content/themes/phodat/build/css
+
+var jsSrc = ["source/js/**/*.js", "!source/js/test/**/*.*"];
+var jsDest = ""; //example www/wp-content/themes/phodat/build/js
+
+var scriptsName = 'scripts.js';
+
+var jQuerySrc = 'node_modules/jquery/dist/*.*';
+var bootstrapSrc = 'node_modules/bootstrap-sass/assets/**/*.*';
+
+//var vendorDest = 'www/wp-content/themes/phodat/build/vendor';
+//var imagesDest = 'www/wp-content/themes/phodat/assets/images';
+
+/* autoprefixer options */
+var autoprefixerOptions = {
+    browsers: ['last 3 version', 'last 15 Chrome versions', 'last 15 Firefox versions', 'last 3 Explorer versions', 'last 3 Edge versions', 'last 3 iOS versions']
+};
+
+
+// 2 - creating tasks
+
+//the default task - when user trigger gulp function
+gulp.task('default', ['copy-vendor-assets', 'sass-dev', 'scripts-dev', 'watch']);
+
+
+// main watch task
+gulp.task('watch', function () {
+
+    browserSync.init({
+        open: "external",
+        proxy: "phodat.dev/"
+        //port: 80
+    });
+
+    gulp.watch(sassSrc, ['sass-dev']);
+    gulp.watch(jsSrc, ['scripts-dev']);
+    gulp.watch(srcRoot).on('change', browserSync.reload);
+});
+
+
+gulp.task('sass-dev', function () {
+    gulp.src(sassSrc)
+            .pipe(sourcemaps.init())
+            .pipe(sass().on('error', sass.logError))
+            .pipe(postcss([autoprefixer(autoprefixerOptions)]))
+            .pipe(sourcemaps.write("."))
+            .pipe(gulp.dest(sassDest));
+});
+
+
+gulp.task('scripts-dev', function () {
+    gulp.src(jsSrc)
+            .pipe(sourcemaps.init())
+//        .pipe(babel())
+            .pipe(concat(scriptsName))
+            .pipe(sourcemaps.write("."))
+            .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('copy-jquery', function () {
+    gulp.src(jQuerySrc).pipe(gulp.dest(vendorDest + '/jquery'));
+});
+
+gulp.task('copy-bootstrap', function () {
+    gulp.src(bootstrapSrc).pipe(gulp.dest(vendorDest + '/bootstrap'));
+});
+
+gulp.task('copy-fonts', function () {
+    gulp.src(fontsSrc).pipe(gulp.dest(fontsDest));
+});
+
+/* SASS LINT */
+gulp.task('sass-lint', function () {
+    gulp.src(sassSrc)
+            .pipe(sassLint())
+            .pipe(sassLint.format())
+            .pipe(sassLint.failOnError())
+});
+
+/* ESLINT */
+gulp.task('eslint', function () {
+    gulp.src(jsSrc)
+            .pipe(eslint())
+            .pipe(eslint.format())
+            .pipe(eslint.failAfterError());
+});
+
+
+// main copy task
+gulp.task('copy-vendor-assets', ['copy-jquery', 'copy-bootstrap', 'copy-fonts']);
